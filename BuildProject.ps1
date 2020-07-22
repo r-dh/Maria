e16ccb63bdc6aea6d7cc2230b4642856d3401528
r_dh <remy.dheygere@howest.be>
2020-07-22 12:00:38 +0200

    fix: Prevented bug that external packages were not correctly found

--- BuildProject.ps1
+++ BuildProject.ps1
@@ -1,6 +1,8 @@
 $vswhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
 
 $mspath = & $vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
+
+& "dotnet" restore
 if ($mspath) {
   & $mspath './Maria.Editor/Maria.Editor.csproj' '/p:Configuration=Release' '/p:Platform=x64'
 } else {
